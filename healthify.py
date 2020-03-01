ccbc897e2083609327fa1df59101b8e92fb5964e
Lyon Zhang <lyonzhang2021@u.northwestern.edu>
2020-03-01 15:08:58 -0600

    healthify draft

---
+++ healthify.py
@@ -0,0 +1,70 @@
+import sys
+import web_scraping
+from healthify_subs import healthy_subs, unhealthy_subs
+
+def healthify(recipe):
+	directions = recipe['directions']
+	ingredients = recipe['ingredients']
+	print("BEFORE")
+	print(directions)
+	print(ingredients)
+	sub_keys = healthy_subs.keys()
+
+	i = 0
+	for base_ing in ingredients:
+		for sub_ing in sub_keys:
+			if sub_ing in base_ing:
+				ingredients[i] = ingredients[i].replace(sub_ing, healthy_subs[sub_ing])
+		i += 1
+
+	i = 0
+	for step in directions:
+		for sub_ing in sub_keys:
+			if sub_ing in step:
+				directions[i] = directions[i].replace(sub_ing, healthy_subs[sub_ing])
+		i += 1
+
+	print("AFTER")
+	print(directions)
+	print(ingredients)
+
+def unhealthify(recipe):
+	directions = recipe['directions']
+	ingredients = recipe['ingredients']
+	print("BEFORE")
+	print(directions)
+	print(ingredients)
+	sub_keys = unhealthy_subs.keys()
+
+	i = 0
+	for base_ing in ingredients:
+		for sub_ing in sub_keys:
+			if sub_ing in base_ing:
+				ingredients[i] = ingredients[i].replace(sub_ing, unhealthy_subs[sub_ing])
+		i += 1
+
+	i = 0
+	for step in directions:
+		for sub_ing in sub_keys:
+			if sub_ing in step:
+				directions[i] = directions[i].replace(sub_ing, unhealthy_subs[sub_ing])
+		i += 1
+
+	print("AFTER")
+	print(directions)
+	print(ingredients)
+
+
+if __name__ == "__main__":
+	if len(sys.argv) == 3:
+		url = sys.argv[1]
+		if url == 'default':
+			url = 'https://www.allrecipes.com/recipe/162760/fluffy-pancakes/'
+		rf = web_scraping.RecipeFetcher()
+		res = rf.scrape_recipe(url)
+		if sys.argv[2] == '0':
+			healthify(res)
+		else:
+			unhealthify(res)
+	else:
+		print("Invalid Number of Arguements. Run by typing 'python3 driver.py a_url'\n")
---
+++ healthify_subs.py
@@ -0,0 +1,11 @@
+healthy_subs = {
+	'butter':'olive oil',
+	'white sugar':'brown sugar',
+	'beef':'lean turkey',
+	'white rice':'quinoa'
+}
+
+unhealthy_subs = {
+	'vegetable oil':'butter',
+	'brown sugar':'white sugar'
+}
