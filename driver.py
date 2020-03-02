2d74d53d5f1a40c8dc046a8154301421f4625f2e
Lyon Zhang <lyonzhang2021@u.northwestern.edu>
2020-03-02 13:45:39 -0600

    created to chinese and linked to driver

--- driver.py
+++ driver.py
@@ -6,6 +6,7 @@ import parse_recipe
 import cooking_method
 import vegetarian
 import make_italian
+import make_chinese
 
 '''
 Driver script to run recipe parsing & transformation interaction with user.
@@ -31,8 +32,8 @@ def main(url):
 	# ask user to select a transformation
 	TRANSFORMATIONS = ['To and from vegetarian (REQUIRED)',
 						'To and from healthy (REQUIRED)', 
-						'Style of cuisine (AT LEAST ONE REQUIRED)', 
-						'Additional Style of cuisine (OPTIONAL)', 
+						'To Italian (AT LEAST ONE REQUIRED)', 
+						'To Chinese (OPTIONAL)', 
 						'DIY to easy (OPTIONAL)', 
 						'Double the amount or cut it by half (OPTIONAL)', 
 						'Cooking method (OPTIONAL)']
@@ -87,7 +88,14 @@ def main(url):
 			print()
 		
 		elif selection == 3:
-			pass
+			new_directions, new_ingredients = make_chinese.chinese(res)
+			res['directions'] = new_directions
+			res['ingredients'] = new_ingredients
+
+
+			print("New Directions: {}".format(new_directions))
+			print("New Ingredients: {}".format(new_ingredients))
+			print()
 		
 		elif selection == 4:
 			pass
---
+++ make_chinese.py
@@ -0,0 +1,130 @@
+from bs4 import BeautifulSoup
+import requests
+
+"""
+
+define meats, herbs, and sauces dictionary
+
+"""
+non_chinese_herbs = ['asafoetida', 'achiote', 'allspice', 'cardamom', 'cayenne',\
+                'gochugaru', 'grains of paradise', 'kaffir', 'loomi', 'mace', 'mahlab',\
+                'nutmeg', 'paprika', 'saffron', 'suman', 'tumeric', 'chervil',\
+                'dill', 'fenugreek', 'lovage', 'marjoram', 'mint',\
+                'oregano', 'thyme', 'bay leaves', 'rosemary', 'sage', \
+                'parsley', 'chili powder', 'chili flakes', 'garlic',\
+                'sumemr savory','shiso', 'tarragon', 'baharat', 'bebere', 'bouquet garni', \
+                'chili powder', 'dukkah', 'garam masala', 'herbes de provence', 'old bay', \
+                'pickling spice', 'ras el hanout', 'shichimi togarashi', "za'atar", "taco seasoning"]
+
+non_chinese_fish = ['salmon', 'tuna','sardine','herring','tilapia','catfish','trout','mackerel','anchovy','perch','pike']
+non_chinese_sauces = ['ginger','salsa','guacamole','picante','tomato','alfredo','mushroom','steak','barbecue','marinara','mayonnaise','tartar','garlic']
+non_chinese_lipids = ['butter','olive oil','coconut oil','avocado oil','canola oil','vegetable oil']
+non_chinese_peppers = ['jalapeno','habanero','chipotle']
+non_chinese_noodles = ['spaghetti noodle','fettuccine noodle','fettuccini noodle','capellini noodle','angel hair noodle','tagliatelle noodle','linguine noodle']
+non_chinese_grains = ['penne','orzo','farfelle','macaroni','rotini','rigatoni','gnocchi']
+
+# create dictionary
+chinese_dict = dict.fromkeys(non_chinese_sauces[:4], 'soy sauce')
+chinese_dict.update(dict.fromkeys(non_chinese_fish[4:8], 'hoison sauce'))
+chinese_dict.update(dict.fromkeys(non_chinese_fish[8:], 'sweet and sour sauce'))
+chinese_dict['browning sauce'] = 'soy sauce'
+chinese_dict['pinto beans'] = 'soybeans'
+chinese_dict['black beans'] = 'soybeans'
+chinese_dict['refried beans'] = 'soybeans'
+chinese_dict['chive'] = 'chinese chive'
+chinese_dict['basil'] = 'thai basil'
+chinese_dict['bison'] = 'beef'
+chinese_dict['sausage'] = 'chinese sausage'
+chinese_dict['vinegar'] = 'rice vinegar'
+chinese_dict.update(dict.fromkeys(non_chinese_grains[:], 'white rice'))
+chinese_dict.update(dict.fromkeys(non_chinese_noodles[:], 'wheat noodle'))
+chinese_dict.update(dict.fromkeys(non_chinese_peppers[:], 'chili'))
+chinese_dict.update(dict.fromkeys(non_chinese_lipids[:], 'sesame oil'))
+chinese_dict.update(dict.fromkeys(non_chinese_fish[:4], 'sea bass'))
+chinese_dict.update(dict.fromkeys(non_chinese_fish[4:8], 'cod'))
+chinese_dict.update(dict.fromkeys(non_chinese_fish[8:], 'haddock'))
+chinese_dict.update(dict.fromkeys(non_chinese_herbs[:5], 'coriander'))
+chinese_dict.update(dict.fromkeys(non_chinese_herbs[5:10], 'thai basil'))
+chinese_dict.update(dict.fromkeys(non_chinese_herbs[10:15], 'lemongrass'))
+chinese_dict.update(dict.fromkeys(non_chinese_herbs[15:20], 'star anise'))
+chinese_dict.update(dict.fromkeys(non_chinese_herbs[20:25], 'fenugreek'))
+chinese_dict.update(dict.fromkeys(non_chinese_herbs[25:30], 'galangal'))
+chinese_dict.update(dict.fromkeys(non_chinese_herbs[30:35], 'cloves'))
+chinese_dict.update(dict.fromkeys(non_chinese_herbs[35:40], 'turmeric'))
+chinese_dict.update(dict.fromkeys(non_chinese_herbs[40:45], 'five-spice'))
+chinese_dict.update(dict.fromkeys(non_chinese_herbs[45:], 'cumin'))
+
+"""
+
+helper functions for making recipe chinese
+
+"""
+def check_corn_beef(string):
+    words = string.lower().split()
+    idx = words.index('beef')
+    if words[idx - 1] == 'corn':
+        return 1
+    if words[idx - 1] == 'corned':
+        return 2
+    else:
+        return 0
+
+def check_sauce(string):
+    words = string.lower().replace(",", "").split()
+    if words.count('sauce') > 1:
+        idx = words.index('sauce')
+        del words[idx]
+        s = " "
+        return s.join(words)
+    else:
+        return string
+
+
+"""
+
+make recipe chinese
+
+"""
+
+def chinese(recipe):
+    directions = recipe['directions']
+    ingredients = recipe['ingredients']
+    sub_keys = chinese_dict.keys()
+
+    i = 0
+    for base_ing in ingredients:
+        base_ing = base_ing.lower()
+        corn = 0
+        for sub_ing in sub_keys:
+            if sub_ing in base_ing:
+                if sub_ing is 'beef':
+                    corn = check_corn_beef(base_ing)
+                if corn == 1:
+                    ingredients[i] = ingredients[i].replace('corn beef', chinese_dict[sub_ing])
+                    break
+                if corn == 2:
+                    ingredients[i] = ingredients[i].replace('corned beef', chinese_dict[sub_ing])
+                    break
+                else:
+                    ingredients[i] = ingredients[i].lower().replace(sub_ing, chinese_dict[sub_ing])
+
+        # remove second 'sauce'
+        if 'sauce' in ingredients[i].lower().replace(",", ""):
+            ingredients[i] = check_sauce(ingredients[i])
+
+        i += 1
+
+    i = 0
+    for step in directions:
+        step = step.lower()
+        for sub_ing in sub_keys:
+            s = step.lower().replace(".", "")
+            if sub_ing in s:
+                directions[i] = directions[i].lower().replace(sub_ing, chinese_dict[sub_ing])
+
+        # remove second 'sauce'
+        if 'sauce' in directions[i].lower():
+            directions[i] = check_sauce(directions[i])
+        i += 1
+
+    return directions, ingredients
