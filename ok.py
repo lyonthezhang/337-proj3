de50e36e2657854010951925ecb426d5cc030a6e
Lyon Zhang <lyonzhang2021@u.northwestern.edu>
2020-03-14 15:14:52 -0500

    few lines

--- ok.py
+++ ok.py
@@ -16,6 +16,7 @@ https://www.allrecipes.com/recipe/8039/bee-sting-cake-bienenstich-ii/
 expletives = ['fart','darn','heck','shut up','stupid','poop']
 retrieval_keywords = ['show','get','display','let me see','gimme','give']
 enumerations = ['first','second','third','fourth','fifth','sixth','seventh','eighth','ninth','tenth','eleventh','twelfth']
+moresteps = ['1st','2nd','3rd','4th','5th','6th','7th','8th','9th','10th','11th','12th']
 steps = ['step 1', 'step 2', 'step 3', 'step 4', 'step 5', 'step 6', 'step 7', 'step 8', 'step 9', 'step 10', 'step 11', 'step 12']
 negation = 'to last'
 
@@ -42,8 +43,21 @@ def directionNavigator(userinput):
 
 	counter = 0
 	for word in enumerations:
-		if word in userinput:
+		if word in inputs:
 			if negation in userinput:
+				res_index = len(res['directions']) - 1 - counter
+			else:
+				res_index = counter
+			print('\nOKBot: {}'.format(res['directions'][res_index]))
+			return
+		counter += 1
+		if counter == len(res['directions']):
+			break
+
+	counter = 0
+	for word in moresteps:
+		if word in inputs:
+			if negation in inputs:
 				res_index = len(res['directions']) - 1 - counter
 			else:
 				res_index = counter
@@ -96,9 +110,6 @@ def directionNavigator(userinput):
 			print("Step {}: {}".format(i + 1, dir[i]))
 		return
 
-	# print('\nOkBot: Here are the directions:\n')
-	# human_readable.print_directions(res, title='')
-
 def parseInput(userinput):
 	global res
 
@@ -135,6 +146,9 @@ def parseInput(userinput):
 			search_base_url = 'https://www.youtube.com/results?search_query=%s'
 			search_url = search_base_url % (userinput.replace(' ','+'))
 			print('\n' + search_url)
+		else:
+			print("\nOkBot: That doesn't seem to be a part of this recipe! Please ask related questions only, my brain isn't huge.\n")
+
 		return
 
 	# 'what is' questions
@@ -146,7 +160,11 @@ def parseInput(userinput):
 			search_base_url = 'https://www.google.com/search?q=%s'
 			search_url = search_base_url % (userinput.replace(' ','+'))
 			print('\n' + search_url)
+		else:
+			print("\nOkBot: That doesn't seem to be a part of this recipe! Please ask related questions only, my brain isn't huge.\n")
 		return
+
+	print("\nOkBot:Hmm, I'm sorry, but it seems I'm not smart enough to handle that. Please try asking me something else!\n")
 
 
 print("\nOkBot: Hello. I am OkBot. How are you? Please ask me to walk you through a recipe.\n")
