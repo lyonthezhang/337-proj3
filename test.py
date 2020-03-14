37c825a039b26fe4940ba94e122fea06404cdd21
Lyon Zhang <lyonzhang2021@u.northwestern.edu>
2020-03-14 13:52:55 -0500

    requests causing heroku to error

--- test.py
+++ test.py
@@ -7,4 +7,5 @@ def testme():
 			https://www.allrecipes.com/recipe/162760/fluffy-pancakes/
 			'''
 	html = requests.get(recipe)
-	return type(html.content)
+	response = "DUMMY: "
+	return response + html.content
