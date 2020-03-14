d71a20dca268dc2c5164eb3ead2f6a64afd05f05
Lyon Zhang <lyonzhang2021@u.northwestern.edu>
2020-03-14 13:45:37 -0500

    testing heroku more

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 import re
+# from bs4 import BeautifulSoup
+import requests
 
 def testme():
+	recipe = '''
+			https://www.allrecipes.com/recipe/162760/fluffy-pancakes/
+			'''
+	html = requests.get(recipe)
 	r = re.compile("([0-9]*\.?[0-9]*)([a-zA-Z]+)")
-	return "HEY IT WORKED LOL"
+	return html
